24711e41d03a8240b9dabbbf831b9467b8085de0
tuananh97 <tuananhk60uet@gmail.com>
2018-04-22 13:45:26 +0700

    a

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Session;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+class AdminController extends Controller
+{
+    public function login(Request $request){
+    if($request->isMethod('post')){
+    $data = $request->input();
+    if(Auth::attempt(['email'=>$data['email'], 'password'=>$data['password'],'admin'=>'1'])){
+       /* Session::put('adminSession',$data['email']);*/
+        return redirect('/admin/dashboard');
+    }else{
+        return redirect('/admin')->with('flash_messsage_error','Invalin');
+      }
+         }
+     return view('admin.admin_login');
+    }
+
+    public function dashboard(){
+       /*if(Session::has('adminSession')){
+
+       }else{
+           return redirect('/admin')->with('flash_message_error','Please login to access');
+       }*/
+        return view('admin.dashboard');
+    }
+    public function settings(){
+        return view('admin.settings');
+    }
+
+    public function checkPassword(Request $request){
+        $data = $request->all();
+        $current_password = $data['current_pwd'];
+        $check_password = User::where(['admin'=>'1'])->first();
+        if(Hash::check($current_password,$check_password->password)){
+            echo "true"; die;
+        }else{
+            echo "false"; die;
+        }
+        
+    }
+    
+    public function updatePassword(Request $request){
+        if($request->isMethod('post')){
+            $data = $request->all();
+            $check_password = User::where(['email'=> Auth::user()->email])->first();
+            $current_password = $data['current_pwd'];
+            if(Hash::check($current_password,$check_password->password)){
+               $password = bcrypt($data['new_pwd']);
+               User::where('id','1')->update(['password'=>$password]);
+               return redirect('/admin/settings')->with('flash_message_success','Cap nhat thanh cong');
+            }else{
+                return redirect('/admin/settings')->with('flash_message_error','Cap nhat that bai');
+            }
+        }
+    }
+    public function logout(){
+       
+        Session::flush();
+        return redirect('/admin')->with('flash_messsage_success','ok');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::match(['get','post'],'/admin','AdminController@login');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['middleware' => ['auth']],function(){
+    Route::get('/admin/dashboard','AdminController@dashboard');
+    Route::get('/admin/settings','AdminController@settings');
+    Route::get('/admin/check-pwd','AdminController@checkPassword');
+    Route::match(['get','post'],'/admin/update-pwd','AdminController@updatePassword');
+});
+
+Route::get('/logout','AdminController@logout');
+
